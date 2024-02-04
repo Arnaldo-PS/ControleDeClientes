2058c70191758da7a5d7d115c72b269b155a3756
Arnaldo Pereira <arnaldopereirasilva99@gmail.com>
2024-02-04 15:26:09 -0300

    feat: configurando conexão com banco de dados

---
+++ ControleContatos/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using ControleContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
