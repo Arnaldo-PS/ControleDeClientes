bc0c303df7d2d166dba07a8c4c338ae84d852277
Arnaldo Pereira <arnaldopereirasilva99@gmail.com>
2024-02-04 21:03:16 -0300

    feat: add validação aos campos de form

--- ControleContatos/Models/ContatoModel.cs
+++ ControleContatos/Models/ContatoModel.cs
@@ -1,10 +1,23 @@
-﻿namespace ControleContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleContatos.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
+
+        // faz com que o campo seja obrigatório
+        [Required(ErrorMessage ="Digite o nome do contato")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o email do contato")]
+        // valida se o email é valido
+        [EmailAddress(ErrorMessage ="Utilize um email válido!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o celular do contato")]
+        // valida se o telefone é valido
+        [Phone(ErrorMessage ="Utilize um número de celular válido!")]
         public string Celular { get; set; }
     }
 }
